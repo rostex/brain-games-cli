2028f87c7dd8ce31878daf703358c35646764bcc
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-03 23:27:31 +0300

    remove Scanner from method

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,8 +7,7 @@ public class Cli {
     public static void greeting() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        Scanner SCANNER = new Scanner(System.in);
-        String userName = SCANNER.next();
+        String userName = App.scanner.next();
         System.out.println("Hello, " + userName);
     }
 }
