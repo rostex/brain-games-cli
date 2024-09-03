ecb235d32448942e6b87fa0ec9c233df4b16d522
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-04 00:29:22 +0300

    optimize code

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,32 +1,44 @@
 package hexlet.code;
 
 public class Even {
+    static int a = 1; // Начальный диапазон случайного числа
+    static int b = 20; // Конечный диапазон случайного числа
+
     public static void evenGame() {
-        Cli.greeting();
+        Cli.greeting(); // Приветствие
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        // First random number
-        // String answerYes = "yes";
-        // String answerNo = "no";
-        int a = 1; // Начальное число диапазона
-        int b = 20; // Конечное число диапазона
+
+        firstQuestion(); // Первый вопрос
+        secondQuestion(); // Второй вопрос
+        thirdQuestion(); // Третий вопрос
+
+    }
+
+    // Первый вопрос
+    public static void firstQuestion() {
         int randomNumber1 = a + (int) (Math.random() * b);
         System.out.println("Question: " + randomNumber1);
         System.out.println("Your answer: ");
         String answer1 = App.scanner.next();
-         if (randomNumber1 % 2 == 0 && "yes".equals(answer1)) {
-             System.out.println("Correct!");
-         } else if (randomNumber1 % 2 != 0 && "no".equals(answer1)) {
+
+        if (randomNumber1 % 2 == 0 && "yes".equals(answer1)) {
+            System.out.println("Correct!");
+        } else if (randomNumber1 % 2 != 0 && "no".equals(answer1)) {
             System.out.println("Correct!");
         } else {
             System.out.println("'" + answer1 + "' is wrong answer ;(. Correct answer was 'no'."); // инверсия к ответу
             System.out.println("Let's try again,"); // добавить юзера
-            System.exit(0);
-         }
-        // Вопрос 2
+            App.main(null);
+        }
+    }
+
+    // Второй вопрос
+    public static void secondQuestion() {
         int randomNumber2 = a + (int) (Math.random() * b);
         System.out.println("Question: " + randomNumber2);
         System.out.println("Your answer: ");
         String answer2 = App.scanner.next();
+
         if (randomNumber2 % 2 == 0 && "yes".equals(answer2)) {
             System.out.println("Correct!");
         } else if (randomNumber2 % 2 != 0 && "no".equals(answer2)) {
@@ -34,10 +46,12 @@ public class Even {
         } else {
             System.out.println("'" + answer2 + "' is wrong answer ;(. Correct answer was 'no'."); // инверсия к ответу
             System.out.println("Let's try again,"); // добавить юзера
-            System.exit(0);
+            App.main(null);
         }
+    }
 
-        // Вопрос 3
+    // Третий вопрос
+    public static void thirdQuestion() {
         int randomNumber3 = a + (int) (Math.random() * b);
         System.out.println("Question: " + randomNumber3);
         System.out.println("Your answer: ");
@@ -45,13 +59,15 @@ public class Even {
         if (randomNumber3 % 2 == 0 && "yes".equals(answer3)) {
             System.out.println("Correct!");
             System.out.println("Congratulations, Bill!");
+            App.main(null);
         } else if (randomNumber3 % 2 != 0 && "no".equals(answer3)) {
             System.out.println("Correct!");
             System.out.println("Congratulations, Bill!");
+            App.main(null);
         } else {
             System.out.println("'" + answer3 + "' is wrong answer ;(. Correct answer was 'no'."); // инверсия к ответу
             System.out.println("Let's try again,"); // добавить юзера
-            System.exit(0);
+            App.main(null);
         }
     }
 }
