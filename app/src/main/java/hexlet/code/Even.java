6ad92db1934bf80f3877d32858553957945853c8
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-05 00:52:04 +0300

    add some alternative logic code in comments

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -32,4 +32,37 @@ public class Even {
         }
     }
 
+//    public static void askQuestion1() {
+//
+//        String operatorSwitch = "null";
+//
+//        int operator = Engine.getRandomNumber(1, 2);
+//        boolean result = false;
+//        int number1 = Engine.getRandomNumber(1, 20);
+//        int number2 = Engine.getRandomNumber(1, 20);
+//
+//        result = switch (operator) {
+//            case 1 -> {
+//                operatorSwitch = "yes";
+//                yield number1 % 2 == 0;
+//            }
+//            case 2 -> {
+//                operatorSwitch = "no";
+//                yield number1 % 2 != 0;
+//            }
+//            default -> result;
+//        };
+//
+//        System.out.println("Question: " + number1);
+//        System.out.println("Your answer: ");
+//        String answer = App.scanner.next();
+//
+//        if (operatorSwitch.equals(answer)) {
+//            System.out.println("Correct!");
+//        } else {
+//            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + operatorSwitch + "'.");
+//            System.out.println("Let's try again,"); // добавить юзера
+//            App.main(null);
+//        }
+//    }
 }
