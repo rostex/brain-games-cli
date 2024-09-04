e48652400b73cadeb1405a5744c05dc5ee9b264b
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-05 00:52:45 +0300

    fix logic code

--- app/src/main/java/hexlet/code/Calc.java
+++ app/src/main/java/hexlet/code/Calc.java
@@ -6,27 +6,52 @@ public class Calc {
         Cli.greeting(); // Greeting
         System.out.println("What is the result of the expression?");
 
-//        askQuestion(); // First question
-//        askQuestion(); // Second question
-//        askQuestion(); // Third question
+        askQuestion(); // First question
+        askQuestion(); // Second question
+        askQuestion(); // Third question
 
         System.out.println("Congratulations, Bill!");
 
     }
 
-    // Question logic
-//    public static void askQuestion() {
-//        System.out.println("Question: " + Engine.getRandomExpression());
-//        System.out.println("Your answer: ");
-//        int answer = Integer.parseInt(App.scanner.next());
-//
-//        if (Engine.getRandomExpression() == answer) {
-//            System.out.println("Correct!");
-//        } else {
-//            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + Engine.getRandomExpression() + "'."); // инверсия к ответу
-//            System.out.println("Let's try again,"); // добавить юзера
-//            App.main(null);
-//        }
-//    }
+    // Calc question logic
 
+    public static void askQuestion() {
+
+        String operatorSwitch = "null";
+
+        int operator = Engine.getRandomNumber(1, 3);
+        int result = 0;
+        int number1 = Engine.getRandomNumber(1, 20);
+        int number2 = Engine.getRandomNumber(1, 20);
+
+        result = switch (operator) {
+            case 1 -> {
+                operatorSwitch = "+";
+                yield number1 + number2;
+            }
+            case 2 -> {
+                operatorSwitch = "-";
+                yield number1 - number2;
+            }
+            case 3 -> {
+                operatorSwitch = "*";
+                yield number1 * number2;
+            }
+            default -> result;
+        };
+
+        System.out.println("Question: " + number1 + operatorSwitch + number2);
+        System.out.println("Your answer: ");
+        int answer = Integer.parseInt(App.scanner.next());
+
+        if (result == answer) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + result + "'.");
+            System.out.println("Let's try again,"); // добавить юзера
+            App.main(null);
+        }
+
+    }
 }
