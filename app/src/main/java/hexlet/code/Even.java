133b6e5b41d542da53dd98384f6ffdc00a32c2eb
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-05 22:11:55 +0300

    optimize code

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -3,66 +3,68 @@ package hexlet.code;
 public class Even {
 
     public static void evenGame() {
-        Cli.greeting(); // Greeting
+        Engine.putWelcomeMessage();
+        Engine.getUserName();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
-        askQuestion(); // First question
-        askQuestion(); // Second question
-        askQuestion(); // Third question
+        putTask(); // First question
+        putTask(); // Second question
+        putTask(); // Third question
 
-        System.out.println("Congratulations, Bill!");
+        Engine.putCongratulationMessage();
 
     }
 
     // Question logic
-    public static void askQuestion() {
+    public static void putTask() {
         int randomNumber = Engine.getRandomNumber(1, 20);
         System.out.println("Question: " + randomNumber);
         System.out.println("Your answer: ");
-        String answer = App.scanner.next();
+        String answer = Engine.scanner.next();
 
         if (randomNumber % 2 == 0 && "yes".equals(answer)) {
-            System.out.println("Correct!");
+            Engine.putCorrectMessage();
         } else if (randomNumber % 2 != 0 && "no".equals(answer)) {
-            System.out.println("Correct!");
+            Engine.putCorrectMessage();
         } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'."); // инверсия к ответу
-            System.out.println("Let's try again,"); // добавить юзера
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'."); // add var correct-number
+            System.out.println("Let's try again,"); // add userName
             App.main(null);
         }
     }
 
-//    public static void askQuestion1() {
-//
-//        String operatorSwitch = "null";
-//
-//        int operator = Engine.getRandomNumber(1, 2);
-//        boolean result = false;
-//        int number1 = Engine.getRandomNumber(1, 20);
-//        int number2 = Engine.getRandomNumber(1, 20);
-//
-//        result = switch (operator) {
-//            case 1 -> {
-//                operatorSwitch = "yes";
-//                yield number1 % 2 == 0;
-//            }
-//            case 2 -> {
-//                operatorSwitch = "no";
-//                yield number1 % 2 != 0;
-//            }
-//            default -> result;
-//        };
-//
-//        System.out.println("Question: " + number1);
-//        System.out.println("Your answer: ");
-//        String answer = App.scanner.next();
-//
-//        if (operatorSwitch.equals(answer)) {
-//            System.out.println("Correct!");
-//        } else {
-//            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + operatorSwitch + "'.");
-//            System.out.println("Let's try again,"); // добавить юзера
-//            App.main(null);
-//        }
-//    }
+    /*
+    public static void putTask1() {
+
+        String operatorSwitch = "null";/
+        int operator = Engine.getRandomNumber(1, 2);
+        boolean result = false;
+        int number1 = Engine.getRandomNumber(1, 20);
+        int number2 = Engine.getRandomNumber(1, 20);
+
+        result = switch (operator) {
+            case 1 -> {
+                operatorSwitch = "yes";
+                yield number1 % 2 == 0;
+            }
+            case 2 -> {
+                operatorSwitch = "no";
+                yield number1 % 2 != 0;
+            }
+            default -> result;
+        };
+
+        System.out.println("Question: " + number1);
+        System.out.println("Your answer: ");
+        String answer = Engine.scanner.next();
+
+        if (operatorSwitch.equals(answer)) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + operatorSwitch + "'.");
+            System.out.println("Let's try again,"); // добавить юзера
+            App.main(null);
+        }
+    }
+    */
 }
