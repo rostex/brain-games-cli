73e0ec19a43c4167230320386b099f7801e307c0
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-05 22:11:35 +0300

    update variable

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -7,7 +7,7 @@ public class Cli {
     public static void greeting() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        String userName = App.scanner.next();
-        System.out.println("Hello, " + userName);
+        String userName = Engine.scanner.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }
