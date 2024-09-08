3cc219c0a9a5d31b5b3088c27b83a6067a1e6143
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-08 20:34:55 +0300

    add progression logic code

--- app/src/main/java/hexlet/code/Progression.java
+++ app/src/main/java/hexlet/code/Progression.java
@@ -6,7 +6,7 @@ public class Progression {
     public static void progressionGame() {
         Engine.putWelcomeMessage();
         Engine.getUserName();
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        System.out.println("What number is missing in the progression?");
 
         putTask(); // First question
         putTask(); // Second question
@@ -19,31 +19,34 @@ public class Progression {
     public static void putTask() {
         int progressionLength = Engine.getRandomNumber(5, 10);
         int firstNumber = Engine.getRandomNumber(3, 9);
-        int[]  progression = new int[progressionLength];
+        int[] progression = new int[progressionLength];
         int range = Engine.getRandomNumber(2, 7);
         progression [0] = firstNumber;
 
-        for (int i = 1; i < progressionLength; i++) {
-            progression[i] = progression[i + 1] + range;
+        for (int i = 1; i < progression.length; i++) {
+            progression[i] = progression[i - 1] + range;
         }
 
-        System.out.println(Arrays.toString(progression));
+        String[] strProgression = new String[progression.length];
+        for (int j = 0; j < progression.length; j++) {
+            strProgression[j] = String.valueOf(progression[j]);
+        }
 
-        /*System.out.println("Question: " + randomNumber);
+        var randomNumberProgression = Engine.getRandomNumber(1, progression.length - 1);
+        strProgression[randomNumberProgression] = "..";
+
+        System.out.println("Question: " + Arrays.toString(strProgression).replaceAll("\\[|\\]|,", ""));
         System.out.println("Your answer: ");
-        String answer = Engine.scanner.next();
+        int answer = Integer.parseInt(Engine.scanner.next());
 
-        if (randomNumber % 2 == 0 && "yes".equals(answer)) {
-            Engine.putCorrectMessage();
-        } else if (randomNumber % 2 != 0 && "no".equals(answer)) {
+        if (answer == progression[randomNumberProgression]) {
             Engine.putCorrectMessage();
         } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'."); // add var correct-number
+            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + progression[randomNumberProgression] + "'."); // add var correct-number
             System.out.println("Let's try again,"); // add userName
             App.main(null);
         }
 
-         */
     }
 
 }
