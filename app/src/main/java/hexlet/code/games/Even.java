b4ee74c4b527e6247b62e6755fa111248111ffc4
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-15 17:03:55 +0300

    updated variables links removed constant variables fixed isEven method access modifier

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,6 +1,7 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Setup;
 
 public class Even {
     private static final String GAME_DISCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
@@ -8,16 +9,16 @@ public class Even {
     private static final int MAX = 20;
 
     public static void startGame() {
-        String[][] gameData = new String[Engine.NUMBER_OF_ROUNDS][2];
+        String[][] gameData = new String[Setup.NUMBER_OF_ROUNDS][2];
 
-        for (int i = 0; i < Engine.NUMBER_OF_ROUNDS; i++) {
+        for (int i = 0; i < Setup.NUMBER_OF_ROUNDS; i++) {
             gameData[i][0] = String.valueOf(Engine.getRandomNumber(MIN, MAX)); // Array with question
             gameData[i][1] = Even.isEven(Integer.parseInt(gameData[i][0])) ? "yes" : "no"; // Array with answer
         }
         Engine.runGame(GAME_DISCRIPTION, gameData);
     }
     // Is even check method
-    public static boolean isEven(int number) {
+    private static boolean isEven(int number) {
         return number % 2 == 0;
     }
 }
