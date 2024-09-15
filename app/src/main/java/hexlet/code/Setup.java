b3d7010466424becec505eb8b1a259e9fea719be
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-15 16:58:08 +0300

    add Setup.java

---
+++ app/src/main/java/hexlet/code/Setup.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+public class Setup {
+    // Context menu constants setups
+    public static final int TOTAL_MENU_NUMBERS = 6;
+    public static final int EXIT = 0;
+    public static final int GREAT = 1;
+    public static final int EVEN = 2;
+    public static final int CALC = 3;
+    public static final int GCD = 4;
+    public static final int PROGRESSION = 5;
+    public static final int PRIME = 6;
+
+    // Engine constants setups
+    public static final int NUMBER_OF_ROUNDS = 3;
+}
