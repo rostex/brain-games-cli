66599118b1e730c4eae7b5ba2f2f9fe9f17d2f1a
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-18 20:57:53 +0300

    fixed context menu logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,6 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
-
 import java.util.Scanner;
 
 public class App {
@@ -13,29 +12,21 @@ public class App {
     public static void main(String[] args) {
         // Greeting
         System.out.println("Please enter the game number and press Enter.");
-
-        int menuNumber = 0;
-        String input;
         Scanner scanner = new Scanner(System.in);
+        int menuNumber;
 
         // Context menu
-        while (!String.valueOf(Setup.TOTAL_MENU_NUMBERS).equals(menuNumber)) {
-            System.out.println("1 - Great");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - GCD");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            System.out.println("Your choice:");
-            input = scanner.next();
+        System.out.println("1 - Great");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice:");
 
-            try {
-                menuNumber = Integer.parseInt(input);
-            } catch (NumberFormatException e) {
-                System.out.println("Incorrect value");
-            }
-
+        do {
+            menuNumber = Integer.parseInt(scanner.next());
             switch (menuNumber) {
                 case Setup.GREAT -> Cli.greeting();
                 case Setup.EVEN -> Even.startGame();
@@ -46,7 +37,8 @@ public class App {
                 case Setup.EXIT -> System.exit(0);
                 default -> System.out.println("Choose correct number");
             }
-        }
+        } while (menuNumber > Setup.TOTAL_MENU_NUMBERS);
         scanner.close();
     }
+
 }
