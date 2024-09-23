3d473e6c2711039966c3808579ea6377cdb3595a
Alexander Nekrasov <rostex2@yandex.ru>
2024-09-23 20:30:31 +0300

    add MathUtils.java

---
+++ app/src/main/java/hexlet/code/MathUtils.java
@@ -0,0 +1,39 @@
+package hexlet.code;
+
+public final class MathUtils {
+
+    private MathUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    // Get random number method
+    public static int getRandomNumber(final int min, final int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    // Is even check method
+    public static boolean isEven(final int number) {
+        return number % 2 == 0;
+    }
+
+    // Get GCD method of Euclid algorithm
+    public static String getGcd(final int a, final int b) {
+        if (b == 0) {
+            return String.valueOf(a);
+        }
+        return getGcd(b, a % b);
+    }
+
+    // Check prime of number method
+    public static boolean isPrime(final int number) {
+        if (number == 1) {
+            return false;
+        }
+        for (int j = 2; j < number; j++) {
+            if (number % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
