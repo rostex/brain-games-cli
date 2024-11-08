a4d5469118823a79a393fa3ea53f15db519e738f
rostex <rostex2@yandex.ru>
2024-11-08 21:33:16 +0300

    Delete

--- app/src/main/java/hexlet/code/Setup.java
+++
@@ -1,9 +0,0 @@
-package hexlet.code;
-
-import java.util.Scanner;
-
-public class Setup {
-    public static final int NUMBER_OF_ROUNDS = 3;
-    public static final Scanner SCANNER = new Scanner(System.in);
-    public static User user = new User();
-}
