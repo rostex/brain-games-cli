0ec9166762a3129a4ded70d52190146c4a8a3923
rostex <rostex2@yandex.ru>
2024-11-08 21:31:26 +0300

    Create Main.java

---
+++ app/src/main/java/hexlet/code/Main.java
@@ -0,0 +1,20 @@
+package hexlet.code;
+
+import hexlet.code.core.Player;
+
+import java.util.Scanner;
+
+import static hexlet.code.menu.Menu.getMainMenu;
+
+public class Main {
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static Player player = new Player();
+    public static int totalScore = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+        Player.selectName();
+        getMainMenu();
+    }
+
+}
