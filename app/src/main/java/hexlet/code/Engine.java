f7d0d9e5c7e06cee1d2b50b4f691c16d948a02a1
rostex <rostex2@yandex.ru>
2024-11-09 00:32:58 +0300

    Update Engine.java
    Add read and write method for yaml files
    Add methods for work with score
    Add method for statistics

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,40 +1,124 @@
 package hexlet.code;
 
-import static hexlet.code.Setup.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import hexlet.code.core.Difficulty;
+import hexlet.code.core.GameData;
+import hexlet.code.menu.Menu;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static hexlet.code.Main.SCANNER;
+import static hexlet.code.Main.player;
+
 
 public class Engine {
 
-    public static void runGame(String gameDescription, String[][] gameData) {
+    public static void runGame(String gameDescription, List<GameData> gameData) {
+        String playerName = player.getName();
 
-        System.out.println("Hello, " + user.getName() + "!");
+        System.out.println("Hello, " + playerName + "!");
         System.out.println(gameDescription);
 
-        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
-            System.out.println("Question: " + gameData[i][0]);
+        for (int i = 0; i < Difficulty.numberOfRounds ; i++) {
+            GameData roundData = gameData.get(i);
+
+            System.out.println("Question: " + roundData.getQuestion());
             System.out.println("Answer: ");
             String userAnswer = SCANNER.next();
 
-            if (userAnswer.equals(gameData[i][1])) {
+            if (userAnswer.equals(roundData.getAnswer())) {
                 System.out.println("Correct!");
+                updateScore(Difficulty.scoreValue, true);
             } else {
                 System.out.println("'" + userAnswer
                         + "' is wrong answer ;(. Correct answer was '"
-                        + gameData[i][1] + "'.");
-                System.out.println("Let's try again, " + user.getName() + "!\n");
-                Menu.getGamesMenu();
+                        + roundData.getAnswer() + "'.");
+                updateScore(Difficulty.scoreValue, false);
             }
         }
-        System.out.println("Congratulations, " + user.getName() + "!\n");
-        Menu.getGamesMenu();
+        System.out.println("Congratulations, " + playerName + "!\n");
+        getTotalScore();
+        Menu.getMainMenu();
     }
 
-    public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
-        if (user.getName() == null) {
-            System.out.println("May I have your name?");
-            user.setName(SCANNER.next());
+    public static void getTotalScore() {
+        try {
+            File file = new File("app/src/main/resources/PlayersStat.yml");
+
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            Map<String, Integer> stats = readStatsFromFile(file, mapper);
+
+            System.out.println("Your current total score: " + stats.get(player.getName()));
+
+        } catch (IOException e) {
+            System.out.println("Error reading stats file: " + e.getMessage());
         }
     }
 
+    public static void updateScore(int value, boolean isAdd) {
+        String playerName = player.getName();
+        int scoreChange = isAdd ? value : -value;
 
+        Main.totalScore += scoreChange;
+
+        try {
+            File file = new File("app/src/main/resources/PlayersStat.yml");
+
+            if (!file.exists()) {
+                File parentDir = new File(file.getParent());
+                if (!parentDir.exists()) {
+                    parentDir.mkdirs();
+                }
+                file.createNewFile();
+            }
+
+                ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            Map<String, Integer> stats = readStatsFromFile(file, mapper);
+
+            stats.merge(playerName, scoreChange, Integer::sum);
+
+            writeStatsToFile(file, mapper, stats);
+
+            String action = isAdd ? "get" : "lose";
+            System.out.println(String.format("You %s %d score(s)", action, value));
+
+        } catch (IOException e) {
+            System.out.println("Error writing to stats file: " + e.getMessage());
+        }
+    }
+
+    public static void getStatistics() throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        Map<String, Integer> stats = mapper.readValue(new File("app/src/main/resources/PlayersStat.yml"), new TypeReference<Map<String, Integer>>() {});
+
+        System.out.println("Top 5 players:");
+        List<Map.Entry<String, Integer>> sortedStats = stats.entrySet()
+                .stream()
+                .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))  // Сортировка по убыванию очков
+                .collect(Collectors.toList());
+
+        int positionNumber = 1;
+        for (var stat : sortedStats) {
+            if (positionNumber > 5) {
+                break;
+            }
+            String statsTable = String.format("%d. %s: %d", positionNumber, stat.getKey(), stat.getValue());
+            System.out.println(statsTable);
+            positionNumber++;
+        }
+    }
+
+    private static Map<String, Integer> readStatsFromFile(File file, ObjectMapper mapper) throws IOException {
+        return mapper.readValue(file, new TypeReference<Map<String, Integer>>() {});
+    }
+
+    private static void writeStatsToFile(File file, ObjectMapper mapper, Map<String, Integer> stats) throws IOException {
+        mapper.writeValue(file, stats);
+    }
 }
