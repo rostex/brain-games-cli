af6ed88ac45ef4f7ff7f981f0ed2b13dca92af8d
rostex <rostex2@yandex.ru>
2024-11-09 00:38:14 +0300

    Create Player.java
    Add method for working with player name

---
+++ app/src/main/java/hexlet/code/core/Player.java
@@ -0,0 +1,23 @@
+package hexlet.code.core;
+
+import static hexlet.code.Main.SCANNER;
+import static hexlet.code.Main.player;
+
+public class Player {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void selectName() {
+        System.out.println("May I have your name?");
+        player.setName(SCANNER.next());
+
+    }
+
+}
