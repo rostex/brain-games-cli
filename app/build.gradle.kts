ae53f5fce955fb4393a5bc0973f46f415ed04f9b
rostex <rostex2@yandex.ru>
2024-11-08 21:30:23 +0300

    Update implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,14 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "hexlet.code.App"
+        )
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -16,6 +24,8 @@ application {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
+    implementation("org.projectlombok:lombok:1.18.34")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
