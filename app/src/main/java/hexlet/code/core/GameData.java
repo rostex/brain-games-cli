d311ad697c187fc9facd49575232fc68ab30f077
rostex <rostex2@yandex.ru>
2024-11-09 17:48:21 +0300

    Update GameData.java
    Add variables for file path and file name

--- app/src/main/java/hexlet/code/core/GameData.java
+++ app/src/main/java/hexlet/code/core/GameData.java
@@ -1,8 +1,14 @@
 package hexlet.code.core;
 
+import hexlet.code.utils.FileUtils;
+
+import java.io.File;
+
 public class GameData {
     private final String question;
     private final String answer;
+    private static final String filename = "PlayersStat.yml";
+    public static final File gameDataFile = FileUtils.getGameFile(filename);
 
     public GameData(String question, String answer) {
         this.question = question;
