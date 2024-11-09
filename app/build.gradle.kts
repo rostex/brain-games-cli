6860476b4aabf85ce59358b55d21e596c99e7ef5
rostex <rostex2@yandex.ru>
2024-11-09 17:43:27 +0300

    Remove unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,6 @@ application {
 
 dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
-    implementation("org.projectlombok:lombok:1.18.34")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
